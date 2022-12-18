b58cd10970ca8f7e947fb5d65901920a1d306801
Michael Barber <60610888+mike-barber@users.noreply.github.com>
2022-12-18 10:48:09 +0000

    fill attempt

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -1,8 +1,19 @@
 use common::*;
 use ndarray::prelude::*;
 use ndarray::Array1;
+use priority_queue::PriorityQueue;
 
 type Pos = Array1<i32>;
+
+const NEIGHBOUR_OFFSETS: [[isize; 3]; 6] = [
+    [-1, 0, 0],
+    [1, 0, 0],
+    [0, -1, 0],
+    [0, 1, 0],
+    [0, 0, -1],
+    [0, 0, 1],
+];
+const DIST_NOT_FOUND: i32 = i32::MAX / 2;
 
 fn parse_input(input: &str) -> Vec<Pos> {
     input
@@ -48,6 +59,28 @@ fn to_addr_checked(pos: &Pos, shape: &[usize]) -> Option<Ix3> {
     Some(Dim(pos_usize))
 }
 
+fn add_address_checked(addr: Ix3, offset: [isize;3], shape:[usize;3]) -> Option<Ix3> {
+    fn add_signed(x: usize, y: isize) -> Option<usize> {
+        let v = x as isize + y;
+        if v < 0 {
+            return None;
+        }
+        Some(v as usize)
+    }
+    
+    let new_addr = [
+        add_signed(addr[0], offset[0])?,
+        add_signed(addr[1], offset[1])?,
+        add_signed(addr[2], offset[2])?,
+    ];
+
+    if new_addr[0] >= shape[0] || new_addr[1] >= shape[1] || new_addr[2] >= shape[2] {
+        return None;
+    }
+
+    Some(Dim(new_addr))
+}
+
 fn count_neighbours(space: &Array3<i32>, pos: &Pos) -> usize {
     let mut neighbours = 0;
     for dim in 0..3 {
@@ -65,7 +98,7 @@ fn count_neighbours(space: &Array3<i32>, pos: &Pos) -> usize {
     neighbours
 }
 
-fn count_open_faces_part2(space: &Array3<i32>, pos: &Pos) -> usize {
+fn count_open_faces_part2_simple(space: &Array3<i32>, pos: &Pos) -> usize {
     let mut empty_faces = 0;
     for dim in 0..3 {
         for offset in [-1, 1] {
@@ -79,12 +112,25 @@ fn count_open_faces_part2(space: &Array3<i32>, pos: &Pos) -> usize {
                     let space_neighbours = count_neighbours(space, &test_pos);
                     if space_neighbours < 6 {
                         empty_faces += 1;
-                    } else {
-                        println!("enclosed point: {test_pos}");
-                    }
+                    } 
                 }
             } else {
                 // also open space - edge of grid
+                empty_faces += 1;
+            }
+        }
+    }
+    empty_faces
+}
+
+fn count_open_faces_part2_filled(space: &Array3<i32>, exterior_reachable: &Array3<i32>, pos: &Pos) -> usize {
+    let shape = space.shape();
+    let shape = [shape[0], shape[1], shape[2]];
+
+    let mut empty_faces = 0;
+    for offset in NEIGHBOUR_OFFSETS {
+        if let Some(addr) = add_address_checked(to_addr(pos), offset, shape) {
+            if space[addr] == 0 && exterior_reachable[addr] < DIST_NOT_FOUND {
                 empty_faces += 1;
             }
         }
@@ -119,14 +165,17 @@ fn part1(points: &[Pos]) -> Option<usize> {
     Some(surface_area)
 }
 
+
+
 // assuming air pockets are single, and not joined initially
-fn part2(points: &[Pos]) -> Option<usize> {
+fn part2_simple(points: &[Pos]) -> Option<usize> {
     // create space matrix
     let extents = max_dims(points)?;
+    // create empty space around right and bottom edge for fill
     let shape = [
-        extents[0] as usize + 1,
-        extents[1] as usize + 1,
-        extents[2] as usize + 1,
+        extents[0] as usize + 2,
+        extents[1] as usize + 2,
+        extents[2] as usize + 2,
     ];
     let mut space: Array3<i32> = Array3::zeros(shape);
 
@@ -139,19 +188,106 @@ fn part2(points: &[Pos]) -> Option<usize> {
     // find all open surfaces
     let mut surface_area = 0;
     for p in points.iter() {
-        let open_faces = count_open_faces_part2(&space, p);
+        let open_faces = count_open_faces_part2_simple(&space, p);
         surface_area += open_faces;
     }
 
     Some(surface_area)
 }
 
+// fill reachable space, then consider which points have faces to it
+fn part2_filled(points: &[Pos]) -> Option<usize> {
+    // create space matrix
+    let extents = max_dims(points)?;
+    // create empty space around right and bottom edge for fill
+    let shape = [
+        extents[0] as usize + 2,
+        extents[1] as usize + 2,
+        extents[2] as usize + 2,
+    ];
+    let mut space: Array3<i32> = Array3::zeros(shape);
+
+    // place all the points
+    for p in points.iter() {
+        let ix = to_addr(p);
+        space[ix] = 1;
+    }
+
+    // fill reachable space
+    let exterior_reachable = fill_reachable_space(&space);
+
+    // find all open surfaces
+    let mut surface_area = 0;
+    for p in points.iter() {
+        let open_faces = count_open_faces_part2_filled(&space, &exterior_reachable, p);
+        surface_area += open_faces;
+    }
+
+    Some(surface_area)
+}
+
+
+// essentially Dijkstra again
+fn fill_reachable_space(space: &Array3<i32>) -> Array3<i32> {
+    let shape = space.shape();
+    let shape = [shape[0], shape[1], shape[2]];
+    let mut dist: Array3<i32> = Array3::zeros(shape);
+
+    // initialise problem
+    let mut q = PriorityQueue::new();
+    for x in 0..shape[0] {
+        for y in 0..shape[1] {
+            for z in 0..shape[2] {
+                let ix = Dim([x, y, z]);
+                dist[ix] = DIST_NOT_FOUND;
+                // queue priority is highest first
+                q.push(ix, i32::MIN);
+            }
+        }
+    }
+    let start = Dim([0, 0, 0]);
+    dist[start] = 0;
+    q.change_priority(&start, 0);
+
+    // update all the reachable nodes
+    while let Some((u, _)) = q.pop() {
+        for offset in &NEIGHBOUR_OFFSETS {
+            // consider valid neighbours still in the queue
+            let v = add_address_checked(u, *offset, shape);
+            if v.is_none() {
+                continue;
+            }
+            let v = v.unwrap();
+            
+            // skip nodes that are part of the lava lump
+            if space[v] == 1 {
+                continue;
+            }
+
+            // record distance
+            if q.get(&v).is_some() {
+                // distance is to current node (u) + 1
+                let alt = dist[u] + 1;
+                if alt < dist[v] {
+                    // update distances to this node
+                    dist[v] = alt;
+                    q.change_priority(&v, -alt);
+                }
+            }
+        }
+    }
+
+    dist
+}
+
 fn main() -> anyhow::Result<()> {
     let points = parse_input(&read_file("day18/input.txt")?);
 
     println!("part1 result = {:?}", part1(&points));
-    println!("part2 result = {:?}", part2(&points));
-    println!("note: 3402 is wrong -- it's too high");
+    println!("part2 simple result = {:?}", part2_simple(&points));
+    println!("part2 filled result = {:?}", part2_filled(&points));
+    println!("note: 3402 is wrong -- it's too high!");
+    println!("note: 2063 is wrong -- it's too low!");
 
     Ok(())
 }
@@ -194,9 +330,16 @@ mod tests {
     }
 
     #[test]
-    fn part2_correct() {
+    fn part2_simple_correct() {
         let input = parse_input(TEST_INPUT);
-        let res = part2(&input).unwrap();
+        let res = part2_simple(&input).unwrap();
+        assert_eq!(res, 58);
+    }
+
+    #[test]
+    fn part2_filled_correct() {
+        let input = parse_input(TEST_INPUT);
+        let res = part2_simple(&input).unwrap();
         assert_eq!(res, 58);
     }
 }
