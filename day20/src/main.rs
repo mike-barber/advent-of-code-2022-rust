c05f4583663f69b98fc1718d14494a6e139019a4
Michael Barber <60610888+mike-barber@users.noreply.github.com>
2022-12-20 08:22:36 +0000

    inputs

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -1,3 +1,13 @@
+use common::AnyResult;
+use itertools::Itertools;
+
+
+fn parse_input(input: &str) -> AnyResult<Vec<i32>> {
+    let vals: Result<Vec<_>,_> = input.lines().map(str::parse::<i32>).collect();
+    Ok(vals?)
+}
+
+
 fn main() {
     
 }
@@ -9,6 +19,17 @@ mod tests {
     use indoc::indoc;
 
     const TEST_INPUT: &str = indoc! {"
-        
+        1
+        2
+        -3
+        3
+        -2
+        0
+        4
     "};
+
+    #[test]
+    fn parse_input_correct() {
+        parse_input(TEST_INPUT).unwrap();
+    }
 }
