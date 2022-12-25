e709258619feb810036119ee35d76c35ca2da025
Michael Barber <60610888+mike-barber@users.noreply.github.com>
2022-12-25 12:14:09 +0000

    notes

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -3,18 +3,18 @@ use std::{cmp::Ordering, ops::Rem};
 use common::{read_file, AnyResult};
 use itertools::Itertools;
 
-const PART2_KEY: i32 = 811589153;
+const PART2_KEY: i64 = 811589153;
 
-fn parse_input(input: &str) -> AnyResult<Vec<i32>> {
-    let vals: Result<Vec<_>, _> = input.lines().map(str::parse::<i32>).collect();
+fn parse_input(input: &str) -> AnyResult<Vec<i64>> {
+    let vals: Result<Vec<_>, _> = input.lines().map(str::parse::<i64>).collect();
     Ok(vals?)
 }
 
-fn mix_once(array: &[i32]) -> Vec<i32> {
+fn mix_once(array: &[i64]) -> Vec<i64> {
     let moves: Vec<_> = array.iter().copied().enumerate().collect();
     let mut array: Vec<_> = array.iter().copied().enumerate().collect();
 
-    fn format_array(arr: &[(usize, i32)]) -> String {
+    fn format_array(arr: &[(usize, i64)]) -> String {
         let mut s = "[".to_string();
         s.push_str(&arr.iter().map(|(_, v)| v.to_string()).join(" "));
         s.push(']');
@@ -33,9 +33,9 @@ fn mix_once(array: &[i32]) -> Vec<i32> {
             .unwrap();
 
         let target_index = if m >= 0 {
-            (current_index as i32 + m).rem(array.len() as i32)
+            (current_index as i64 + m).rem(array.len() as i64)
         } else {
-            (current_index as i32 + m - 1).rem_euclid(array.len() as i32)
+            (current_index as i64 + m - 1).rem_euclid(array.len() as i64)
         };
         let target_index = target_index as usize;
 
@@ -63,12 +63,12 @@ fn mix_once(array: &[i32]) -> Vec<i32> {
     array.iter().map(|(_, v)| v).copied().collect()
 }
 
-fn calculate_permutations_old(moves: &[i32]) -> Vec<usize> {
+fn calculate_permutations_old(moves: &[i64]) -> Vec<usize> {
     let mut positions: Vec<usize> = (0..moves.len()).collect();
     println!("{:?}", permute(moves, &positions));
     for (idx, mv) in moves.iter().enumerate() {
         let curr_index = positions.iter().position(|&p| p == idx).unwrap();
-        let dest_index = (curr_index as i32 + mv).rem_euclid(moves.len() as i32) as usize;
+        let dest_index = (curr_index as i64 + mv).rem_euclid(moves.len() as i64) as usize;
 
         println!("move {mv}: {curr_index}->{dest_index}");
         let left = curr_index.min(dest_index);
@@ -95,7 +95,7 @@ fn calculate_permutations_old(moves: &[i32]) -> Vec<usize> {
     positions
 }
 
-fn calculate_permutations(moves: &[i32]) -> Vec<usize> {
+fn calculate_permutations(moves: &[i64]) -> Vec<usize> {
     let len = moves.len();
     let mut positions: Vec<usize> = (0..moves.len()).collect();
     // println!("{:?}", permute(moves, &positions));
@@ -105,8 +105,8 @@ fn calculate_permutations(moves: &[i32]) -> Vec<usize> {
         // println!("mv {mv}");
         if *mv > 0 {
             for i in 0..*mv {
-                let a = (curr_index as i32 + i).rem_euclid(len as i32) as usize;
-                let b = (curr_index as i32 + i + 1).rem_euclid(len as i32) as usize;
+                let a = (curr_index as i64 + i).rem_euclid(len as i64) as usize;
+                let b = (curr_index as i64 + i + 1).rem_euclid(len as i64) as usize;
                 positions.swap(a, b);
                 // println!("  {i} {:?}", permute(moves, &positions));
             }
@@ -114,16 +114,10 @@ fn calculate_permutations(moves: &[i32]) -> Vec<usize> {
 
         if *mv < 0 {
             for i in (*mv..0).rev() {
-                let a = (curr_index as i32 + i).rem_euclid(len as i32) as usize;
-                let b = (curr_index as i32 + i + 1).rem_euclid(len as i32) as usize;
+                let a = (curr_index as i64 + i).rem_euclid(len as i64) as usize;
+                let b = (curr_index as i64 + i + 1).rem_euclid(len as i64) as usize;
                 positions.swap(a, b);
                 // println!("  {i} {:?}", permute(moves, &positions));
-            }
-
-            // this only seems to be required to make the test pass; we still get the right
-            // answer for part1 without it.
-            if curr_index as i32 + mv < 0 {
-                positions.rotate_left(1);
             }
         }
 
@@ -132,7 +126,7 @@ fn calculate_permutations(moves: &[i32]) -> Vec<usize> {
     positions
 }
 
-fn calculate_permutations_fast(moves: &[i32], echo: bool) -> Vec<usize> {
+fn calculate_permutations_fast(moves: &[i64], echo: bool) -> Vec<usize> {
     let len = moves.len();
     let mut positions: Vec<usize> = (0..moves.len()).collect();
 
@@ -156,8 +150,8 @@ fn calculate_permutations_fast(moves: &[i32], echo: bool) -> Vec<usize> {
         let curr_idx = positions.iter().position(|&p| p == orig_idx).unwrap();
 
         let tgt_index = match mv {
-            mv if *mv > 0 => (curr_idx as i32 + *mv + 1).rem_euclid(len as i32) as usize,
-            mv if *mv < 0 => (curr_idx as i32 + *mv).rem_euclid(len as i32) as usize,
+            mv if *mv > 0 => (curr_idx as i64 + *mv + 1).rem_euclid(len as i64) as usize,
+            mv if *mv < 0 => (curr_idx as i64 + *mv).rem_euclid(len as i64) as usize,
             _ => curr_idx,
         };
         let tgt_pos = positions[tgt_index];
@@ -208,19 +202,21 @@ fn calculate_permutations_fast(moves: &[i32], echo: bool) -> Vec<usize> {
     positions
 }
 
-fn permute(array: &[i32], positions: &[usize]) -> Vec<i32> {
+fn permute(array: &[i64], positions: &[usize]) -> Vec<i64> {
     positions.iter().map(|ix| array[*ix]).collect()
 }
 
-fn mix_once_permute(array: &[i32]) -> Vec<i32> {
+fn mix_once_permute(array: &[i64]) -> Vec<i64> {
     let positions = calculate_permutations(array);
     permute(&array, &positions)
 }
 
-fn part1(array: &[i32]) -> i32 {
+fn part1(array: &[i64]) -> i64 {
     //let mixed = mix_once(array);
     let mixed = mix_once_permute(array);
 
+    // note: position of zero is the start; it doesn't matter what overall
+    // rotation the array has, as we're starting the cycle here.
     let pos_zero = mixed.iter().position(|a| *a == 0).unwrap();
 
     let mut sum = 0;
@@ -233,12 +229,14 @@ fn part1(array: &[i32]) -> i32 {
     sum
 }
 
-fn part1_alt(array: &[i32]) -> i32 {
+fn part1_alt(array: &[i64]) -> i64 {
     //let mixed = mix_once(array);
     //let mixed = mix_once_permute(array);
     let positions = calculate_permutations_fast(array, false);
     let mixed = permute(array, &positions);
 
+    // note: position of zero is the start; it doesn't matter what overall
+    // rotation the array has, as we're starting the cycle here.
     let pos_zero = mixed.iter().position(|a| *a == 0).unwrap();
 
     let mut sum = 0;
@@ -263,6 +261,32 @@ fn main() -> AnyResult<()> {
     Ok(())
 }
 
+// Assert that arrays are equal for any rotation of the right
+// array; i.e. the elements need to be in the correct (cyclical) order,
+// but not necessarily starting at the same location.
+fn assert_eq_rotate<T>(left: &[T], right: &[T])
+where
+    T: PartialEq + std::fmt::Debug + Clone,
+{
+    if !check_eq_rotate(left, right) {
+        assert_eq!(left, right);
+    }
+}
+
+fn check_eq_rotate<T>(left: &[T], right: &[T]) -> bool
+where
+    T: PartialEq + std::fmt::Debug + Clone,
+{
+    let mut right = right.iter().cloned().collect_vec();
+    for _ in 0..left.len() {
+        if left == right.as_slice() {
+            return true;
+        }
+        right.rotate_left(1);
+    }
+    return false;
+}
+
 fn scratch() {
     let mut moves = [0; 6];
     println!("positive moves --------");
@@ -270,14 +294,14 @@ fn scratch() {
         moves[2] = m;
         let perm = calculate_permutations(&moves);
         let alt = calculate_permutations_fast(&moves, false);
-        println!("{m}: {perm:?} {alt:?} {}", perm == alt);
+        println!("{m}: {perm:?} {alt:?} {}", check_eq_rotate(&perm, &alt));
     }
     println!("negative moves --------");
     for m in 0..=6 * 6 {
         moves[2] = -m;
         let perm = calculate_permutations(&moves);
         let alt = calculate_permutations_fast(&moves, false);
-        println!("{m}: {perm:?} {alt:?} {}", perm == alt);
+        println!("{m}: {perm:?} {alt:?} {}", check_eq_rotate(&perm, &alt));
     }
 
     // let example = [1, 2, -3, 3, -2, 0, 4];
@@ -315,7 +339,7 @@ mod tests {
     fn permute_mix_correct() {
         let input = parse_input(TEST_INPUT).unwrap();
         let res = mix_once_permute(&input);
-        assert_eq!(res, [1, 2, -3, 4, 0, 3, -2]);
+        assert_eq_rotate(&res, &[1, 2, -3, 4, 0, 3, -2]);
     }
 
     #[test]
@@ -323,7 +347,7 @@ mod tests {
         let input = parse_input(TEST_INPUT).unwrap();
         let positions = calculate_permutations_fast(&input, true);
         let res = permute(&input, &positions);
-        assert_eq!(res, [1, 2, -3, 4, 0, 3, -2]);
+        assert_eq_rotate(&res, &[1, 2, -3, 4, 0, 3, -2]);
     }
 
     #[test]
